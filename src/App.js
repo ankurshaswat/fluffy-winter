2c2979fe78ad38840955936ae57a8365026f27f7
Shashwat Shivam <ankurshaswat@yahoo.co.in>
2019-02-27 16:45:24 +0530

    Fetching code added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: 'Loading',
+    };
+  }
+
+  componentDidMount() {
+    const API = 'https://hn.algolia.com/api/v1/search?query=redux';
+
+    fetch(API)
+      .then(response => response.json())
+      .then(data => this.setState({ data }));
+  }
+
   render() {
+    const { data } = this.state;
+    console.log(data);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {JSON.stringify(this.state.data)}
       </div>
     );
   }
